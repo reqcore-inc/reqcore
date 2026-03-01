2b61a91fd94fad261e7000e594ceb6b9a3cd9221
Joachim <joachim.l.kolle.pers@gmail.com>
2026-03-01 10:48:54 +0100

    added eagle icon to the browser tab

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -74,8 +74,8 @@ export default defineNuxtConfig({
     head: {
       titleTemplate: '%s — Reqcore',
       link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-        { rel: 'icon', type: 'image/png', href: '/eagle-mascot-logo.png' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
       ],
       meta: [
