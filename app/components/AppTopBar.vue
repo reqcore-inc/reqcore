49976c38d587ee05c09b59dd2226632529cadb33
JoachimLK <joachim.l.kolle.pers@gmail.com>
2026-03-11 19:59:35 +0100

    feat(AppTopBar): remove unused transition classes for user menu

--- app/components/AppTopBar.vue
+++ app/components/AppTopBar.vue
@@ -294,9 +294,6 @@ onUnmounted(() => document.removeEventListener('click', onClickOutsideUser))
       enter-active-class="transition duration-200 ease-out"
       enter-from-class="opacity-0 -translate-y-1"
       enter-to-class="opacity-100 translate-y-0"
-      leave-active-class="transition duration-150 ease-in"
-      leave-from-class="opacity-100 translate-y-0"
-      leave-to-class="opacity-0 -translate-y-1"
     >
       <div
         v-if="activeJobId"
